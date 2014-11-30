0ada9c7d7019efc8722fd3db4f4d8679bb80f63c
Anand Thakker <vestibule@anandthakker.net>
2014-11-30 16:04:13 +0100

    circle circle intersection

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 
 let {P, Circle} = require('./lib/geometry'),
     sweep = require('./lib/sweepline'),
-    renderer = require('./lib/render-canvas')
+    renderer = require('./lib/render-canvas'),
+    {intersectCircleCircle} = require('./lib/intersection');
 
 function init() {
   let canvas = document.querySelector('canvas')
@@ -12,12 +13,14 @@ function init() {
   
   let w = canvas.getBoundingClientRect().width;
   let h = canvas.getBoundingClientRect().height;
-  objects.push(new Circle(P(300,500),100));
+  objects.push(new Circle(P(300,500),300));
   objects.push(new Circle(P(650,300),200));
   
   render(objects);
   
-  sweep(canvas.getBoundingClientRect(), 10, objects, render);
+  let isect = intersectCircleCircle(objects[0], objects[1]);
+  render(isect);
+  // sweep(canvas.getBoundingClientRect(), 10, objects, render);
 }
 
 document.addEventListener('DOMContentLoaded', init)
