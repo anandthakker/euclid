8682ec863f9b64f120b8502169a93edf0886d042
Anand Thakker <vestibule@anandthakker.net>
2014-11-30 18:30:57 +0100

    circle-segment and segment-segment intersection

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-let {P, Circle} = require('./lib/geometry'),
+let {P, Circle, Line, Segment} = require('./lib/geometry'),
     sweep = require('./lib/sweepline'),
     renderer = require('./lib/render-canvas'),
-    {intersectCircleCircle} = require('./lib/intersection');
+    {intersect} = require('./lib/intersection');
 
 function init() {
   let canvas = document.querySelector('canvas')
@@ -11,16 +11,24 @@ function init() {
   
   let objects = [];
   
-  let w = canvas.getBoundingClientRect().width;
-  let h = canvas.getBoundingClientRect().height;
   objects.push(new Circle(P(300,500),300));
   objects.push(new Circle(P(650,300),200));
+  objects.push(new Line(P(300, 200), P(600, 500)));
+  objects.push(new Line(P(400, 500), P(700, 200)));
   
   render(objects);
   
-  let isect = intersectCircleCircle(objects[0], objects[1]);
-  render(isect);
-  // sweep(canvas.getBoundingClientRect(), 10, objects, render);
+  let finite = objects.map(obj=>
+    (obj instanceof Line) ? Segment.clip(canvas.getBoundingClientRect(), obj) : obj);
+  let intersections = [];
+  for(let i = 0; i < finite.length; i++) {
+    for(let j = i+1; j < finite.length; j++) {
+      let isect = intersect(finite[i], finite[j]);
+      Array.prototype.push.apply(intersections, isect)
+    }
+  }
+  
+  intersections.forEach(render);
 }
 
 document.addEventListener('DOMContentLoaded', init)
