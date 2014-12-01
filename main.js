d01347c9c9bae0d0c62a03137b2b8b066a4ce888
Anand Thakker <vestibule@anandthakker.net>
2014-12-01 02:32:13 +0100

    basic interaction with control points

--- main.js
+++ main.js
@@ -1,4 +1,6 @@
 
+let d3 = require('d3');
+
 let {P, Circle, Line, Segment} = require('./lib/geometry'),
     sweep = require('./lib/sweepline'),
     render = require('./lib/render-d3-svg'),
@@ -9,7 +11,7 @@ function init() {
   
   let {x: left, y: top, width, height} = svg.viewBox.baseVal;
   let bounds = { left, top, width, height, right: left+width, bottom: top+height };
-  console.log(bounds);
+  
   
   let objects = [];
   
@@ -18,18 +20,32 @@ function init() {
   objects.push(new Line(P(200, 100), P(700, 600)));
   objects.push(new Segment(P(400, 500), P(700, 200)));
   
-  let finite = objects.map(obj=>
-    (obj instanceof Line) ? Segment.clip(bounds, obj) : obj);
-  let intersections = [];
-  for(let i = 0; i < finite.length; i++) {
-    for(let j = i+1; j < finite.length; j++) {
-      let isect = intersect(finite[i], finite[j]);
-      Array.prototype.push.apply(objects, isect)
+  function intersections(objects) {
+    let finite = objects.map(obj=>
+      (obj instanceof Line) ? Segment.clip(bounds, obj) : obj);
+    let intersections = [];
+    for(let i = 0; i < finite.length; i++) {
+      for(let j = i+1; j < finite.length; j++) {
+        let isect = intersect(finite[i], finite[j]);
+        Array.prototype.push.apply(intersections, isect)
+      }
     }
+    return intersections;
   }
   
-  console.log(svg);
-  render(svg, objects);
+  let update = render(svg, onDrag);
+  
+  function onDrag(d) {
+    d.x = Math.max(5, Math.min(bounds.width - 5, d3.event.x))
+    d.y = Math.max(5, Math.min(bounds.height - 5, d3.event.y))
+    // console.log(d);
+    // d3.select(this)
+    // .attr("cx", d.x = Math.max(5, Math.min(bounds.width - 5, d3.event.x)))
+    // .attr("cy", d.y = Math.max(5, Math.min(bounds.height - 5, d3.event.y)));
+    update(objects.concat(intersections(objects)));
+  }
+  
+  update(objects.concat(intersections(objects)));
 }
 
 document.addEventListener('DOMContentLoaded', init)
